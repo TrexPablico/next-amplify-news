f711afcaf614e5c5c3bce172ef660532f40bb2fd
TrexPablico <trexpablico@gmail.com>
2025-02-11 10:29:41 +0800

    contact
    contact form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
           <div className="text-center mt-8">
             <button
               onClick={handleLoadMore}
-              className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-300"
+              className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-400"
             >
               Load More
             </button>
